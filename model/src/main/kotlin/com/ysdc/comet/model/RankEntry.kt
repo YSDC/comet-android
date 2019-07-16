703c7bf936fdb5e8816961bcee0f6f143f7f0b01
David Johannot <djohannotapp@gmail.com>
2019-07-17 00:15:09 +0400

    ranking mapper

--- model/src/main/kotlin/com/ysdc/comet/model/RankEntry.kt
+++ model/src/main/kotlin/com/ysdc/comet/model/RankEntry.kt
@@ -7,30 +7,59 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @JsonClass(generateAdapter = true)
-data class RankEntry (
+data class RankEntry(
     @field:Json(name = "team_id")
-    val teamId : Int,
-    @field:Json(name = "team_name")
-    val teamName : String,
+    val teamId: Int,
     @field:Json(name = "rank")
-    var rank : Int,
+    val rank: Int,
     @field:Json(name = "picture")
-    var picture : String,
+    val picture: String?,
+    @field:Json(name = "team_name")
+    val teamName: String,
     @field:Json(name = "played")
-    var played : Int,
+    val played: Int,
     @field:Json(name = "win")
-    var win : Int,
+    val win: Int,
     @field:Json(name = "winOverTime")
-    var winOverTime : Int,
+    val winOverTime: Int,
     @field:Json(name = "looseOverTime")
-    var looseOverTime : Int,
+    val looseOverTime: Int,
     @field:Json(name = "loose")
-    var loose : Int,
+    val loose: Int,
     @field:Json(name = "goals")
-    var goals : String,
+    val goals: String,
     @field:Json(name = "goalsDiff")
-    var goalsDiff : Int,
+    val goalsDiff: Int,
     @field:Json(name = "points")
-    var points : Int
-): Parcelable
+    val points: Int
+) : Parcelable {
+    data class Builder(
+        var teamId: Int = 0,
+        var teamName: String = "",
+        var rank: Int = 1,
+        var picture: String? = null,
+        var played: Int = 0,
+        var win: Int = 0,
+        var winOverTime: Int = 0,
+        var looseOverTime: Int = 0,
+        var loose: Int = 0,
+        var goals: String = "0:0",
+        var goalsDiff: Int = 0,
+        var points: Int = 0
+    ) {
+        fun teamId(value: Int) = apply { this.teamId = value }
+        fun teamName(value: String) = apply { this.teamName = value }
+        fun rank(value: Int) = apply { this.rank = value }
+        fun picture(value: String) = apply { this.picture = value }
+        fun played(value: Int) = apply { this.played = value }
+        fun win(value: Int) = apply { this.win = value }
+        fun winOverTime(value: Int) = apply { this.winOverTime = value }
+        fun looseOverTime(value: Int) = apply { this.looseOverTime = value }
+        fun loose(value: Int) = apply { this.loose = value }
+        fun goals(value: String) = apply { this.goals = value }
+        fun goalsDiff(value: Int) = apply { this.goalsDiff = value }
+        fun points(value: Int) = apply { this.points = value }
+        fun build() = RankEntry(teamId, teamName, rank, picture, played, win, winOverTime, looseOverTime, loose, goals, goalsDiff, points)
+    }
+}
 
--- network/src/main/kotlin/com/ysdc/comet/network/mapper/RankingMapper.kt
+++ network/src/main/kotlin/com/ysdc/comet/network/mapper/RankingMapper.kt
@@ -7,9 +7,30 @@ import com.ysdc.comet.network.response.RankingResponse
 class RankingMapper {
     fun mapRanking(rankingResponse: RankingResponse) : List<RankEntry>{
         val rankEntries : MutableList<RankEntry> = ArrayList()
-        rankingResponse.entries?.forEach {entry ->
-            teams.add(Team(entry.attributes.teamId, entry.teamName))
+        if(rankingResponse.data?.regions?.size == 1){
+            rankingResponse.data?.regions!![0].rows?.forEach {rank ->
+                if(rank.cells?.size == 10){
+                    val entry = RankEntry.Builder()
+                        .teamId(rank.rowData!!.team!!.id!!)
+                        .rank(cellContentAtIndex(rank,0).toInt())
+                        .picture(rank.cells!![1].image!!.url!!)
+                        .teamName(cellContentAtIndex(rank,2))
+                        .played(cellContentAtIndex(rank,3).toInt())
+                        .win(cellContentAtIndex(rank,4).toInt())
+                        .winOverTime(cellContentAtIndex(rank,5).toInt())
+                        .looseOverTime(cellContentAtIndex(rank,6).toInt())
+                        .loose(cellContentAtIndex(rank,7).toInt())
+                        .goals(cellContentAtIndex(rank,8))
+                        .goalsDiff(cellContentAtIndex(rank,9).toInt())
+
+
+                }
+            }
         }
         return rankEntries
+    }
+
+    private fun cellContentAtIndex(row: RankingResponse.Data.Regions.Row, index : Int) : String{
+        return row.cells!![index].text!![0]
     }
 }
